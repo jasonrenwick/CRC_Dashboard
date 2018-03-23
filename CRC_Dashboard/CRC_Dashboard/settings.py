95ec6a20bd73b5ef9cec70455313820b079fc421
Jason Home <jason.renwick@my.uwi.edu>
2018-03-23 04:21:49 -0400

    inspect model

--- CRC_Dashboard/CRC_Dashboard/settings.py
+++ CRC_Dashboard/CRC_Dashboard/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'CRC_Dashboard'
+    'dashboard.apps.DashboardConfig'
     ]
 
 MIDDLEWARE = [
--- CRC_Dashboard/dashboard/models.py
+++ CRC_Dashboard/dashboard/models.py
@@ -1,14 +1,7 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
-
 
 class FarmBenExpec(models.Model):
     idfarm_ben_expec = models.AutoField(db_column='idFarm_Ben_Expec', primary_key=True)  # Field name made lowercase.
@@ -18,4 +11,3 @@ class FarmBenExpec(models.Model):
     class Meta:
         managed = False
         db_table = 'farm_ben_expec'
-
