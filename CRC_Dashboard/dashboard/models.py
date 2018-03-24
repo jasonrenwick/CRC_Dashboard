a5792d13a1ec2bc9532e482d2f4c68927ef7ac26
Jason Home <jason.renwick@my.uwi.edu>
2018-03-24 15:15:19 -0400

    model linked 3

--- CRC_Dashboard/dashboard/models.py
+++ CRC_Dashboard/dashboard/models.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 
-class FarmBenExpec(models.Model):
-    idfarm_ben_expec = models.AutoField(db_column='idFarm_Ben_Expec', primary_key=True)  # Field name made lowercase.
-    flavour = models.IntegerField(db_column='Flavour', blank=True, null=True)  # Field name made lowercase.
-    consistency = models.IntegerField(db_column='Consistency', blank=True, null=True)  # Field name made lowercase.
+class Scores(models.Model):
+    farmerid = models.IntegerField(primary_key=True, max_length=1)
+    flavour = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'scores'
 
     def __str__(self):
         return self.idfarm_ben_expec
